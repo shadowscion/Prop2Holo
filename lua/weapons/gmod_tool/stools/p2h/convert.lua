060559fe90eab883809fa310bcdc880c9173840f
shadowscion <khaotix2@gmail.com>
2015-08-07 22:20:31 -0500

    Initial Commit

---
+++ lua/weapons/gmod_tool/stools/p2h/convert.lua
@@ -0,0 +1,263 @@
+
+-------------------------------------------
+-- Prop to holo converter
+-- by shadowscion
+if not CLIENT then return end
+
+local string = string
+
+local str_Holo = "    HN++,HT[HN,array] = array(%d,vec(%f,%f,%f),ang(%f,%f,%f),\"%s\",\"%s\",vec4(%d,%d,%d,%d))\n"
+local str_Clip = "    CN++,CT[CN,array] = array(%d,%d,vec(%f,%f,%f),vec(%f,%f,%f))\n"
+
+local str_Header = [[
+@name <NAME>
+@persist [ID SpawnStatus CoreStatus]:string [HT CT]:table [HN CN SpawnCounter ScaleFactor ToggleColMat ToggleShading]
+
+if (first()) {
+    #Settings
+    ScaleFactor   = 1 #scales the contraption
+    ToggleColMat  = 1 #disables materials and color
+    ToggleShading = 0 #disables shading
+
+
+    #Holo data
+]]
+
+local str_Footer = [[
+
+
+    #[
+        HOLOGRAM LOADER - DO NOT EDIT BELOW THIS LINE
+
+        IF YOU WISH TO EDIT HOLOGRAMS AFTER SPAWNING, PLACE CODE AFTER THE
+
+        elseif (CoreStatus == "InitPostSpawn") {
+
+        CODEBLOCK AT THE BOTTOM
+    ]#
+
+
+    function array:holo() {
+        local Index = This[1, number]
+        local Parent = Index != 1 ? holoEntity(1) : entity()
+        local Rescale = vec(ScaleFactor)
+
+        holoCreate(Index, Parent:toWorld(This[2, vector] * ScaleFactor), Rescale, Parent:toWorld(This[3, angle]), vec(255), This[4, string] ?: "cube")
+        holoParent(Index, Parent)
+
+        if (ToggleColMat) {
+            holoMaterial(Index, This[5, string])
+            holoColor(Index, This[6, vector4])
+        }
+
+        if (ToggleShading) { holoDisableShading(Index, 1) }
+    }
+
+    function array:clip() {
+        holoClipEnabled(This[1, number], This[2, number], 1)
+        holoClip(This[1, number], This[2, number], This[3, vector] * ScaleFactor, This[4, vector], 0)
+    }
+
+    function loadContraption() {
+        switch (SpawnStatus) {
+            case "InitSpawn",
+                if (clk("Start")) {
+                    SpawnStatus = "LoadHolograms"
+                }
+                soundPlay("Blip", 0, "@^garrysmod/content_downloaded.wav", 0.212)
+            break
+
+            case "LoadHolograms",
+                while (perf() & holoCanCreate() &  SpawnCounter < HN) {
+                    SpawnCounter++
+                    HT[SpawnCounter, array]:holo()
+
+                    if (SpawnCounter >= HN) {
+                        SpawnStatus = CN > 0 ? "LoadClips" : "PrintStatus"
+                        SpawnCounter = 0
+                        break
+                    }
+                }
+            break
+
+            case "LoadClips",
+                while (perf() & SpawnCounter < CN) {
+                    SpawnCounter++
+                    CT[SpawnCounter, array]:clip()
+
+                    if (SpawnCounter >= CN) {
+                        SpawnStatus = "PrintStatus"
+                        SpawnCounter = 0
+                        break
+                    }
+                }
+            break
+
+            case "PrintStatus",
+                printColor( vec(125,255,125), "HoloCore: ", vec(255,255,255), "Loaded " + HN + " holograms and " + CN + " clips." )
+
+                CoreStatus = "InitPostSpawn"
+                SpawnStatus = ""
+            break
+        }
+    }
+
+    runOnTick(1)
+    timer("Start", 500)
+
+    CoreStatus = "InitSpawn"
+    SpawnStatus = "InitSpawn"
+
+}
+
+#----------------------
+#-- Load the hologram and clip data arrays.
+elseif (CoreStatus == "InitSpawn") {
+    loadContraption()
+}
+
+
+#----------------------
+#-- This is like if (first()) { }, code here is run only once.
+elseif (CoreStatus == "InitPostSpawn") {
+    CoreStatus = "RunThisCode"
+
+    runOnTick(0)
+}
+
+
+#----------------------
+#-- This is where executing code goes
+elseif (CoreStatus == "RunThisCode") {
+
+}
+]]
+
+
+-------------------------------------------
+-- FUNC: Returns table of entity info
+-- ARGS: base entity, selection table
+local function SetupEntityInfo( base, ents )
+    local ret = {}
+
+    for _, ent in ipairs( ents ) do
+        local entry = {
+            lpos = base:WorldToLocal( ent:GetPos() ),
+            lang = base:WorldToLocalAngles( ent:GetAngles() ),
+            model = ent:GetModel(),
+            material = ent:GetMaterial(),
+            color = ent:GetColor(),
+        }
+
+        if not ent.ClipData then ret[#ret + 1] = entry continue end
+
+        -- visclip support ( requires wrex's workshop version )
+        entry.clips = {}
+
+        for _, clip in ipairs( ent.ClipData ) do
+            entry.clips[#entry.clips + 1] = {
+                ldir = clip[1]:Forward(),
+                lpos = clip[1]:Forward()*clip[2],
+            }
+        end
+
+        ret[#ret + 1] = entry
+    end
+
+    return ret
+end
+
+-------------------------------------------
+-- FUNC: Returns formatted e2 code
+-- ARGS: script name, entity info table
+local function FormatEntityInfo( name, info )
+    local ret = string.Replace( str_Header, "<NAME>", name or "DefaultName" )
+
+    for i, entry in pairs( info ) do
+        local line = string.format(
+            str_Holo, i,
+            entry.lpos.x, entry.lpos.y, entry.lpos.z,
+            entry.lang.p, entry.lang.y, entry.lang.r,
+            entry.model, entry.material,
+            entry.color.r, entry.color.g, entry.color.b, entry.color.a
+        )
+
+        if not entry.clips then ret = ret .. line continue end
+
+        -- visclip support ( requires wrex's workshop version )
+        line = line .. "\n    #Clip data <" .. i .. ">\n"
+
+        for ci, clip in ipairs( entry.clips ) do
+            line = line .. string.format(
+                str_Clip, i, ci,
+                clip.lpos.x, clip.lpos.y, clip.lpos.z,
+                clip.ldir.x, clip.ldir.y, clip.ldir.z
+            )
+        end
+
+        if i ~= #info then line = line .. "\n    #Holo data\n" end
+        ret = ret .. line
+    end
+
+    ret = ret .. str_Footer
+
+    return ret
+end
+
+-------------------------------------------
+-- FUNC: Handles data recieved from server
+-- ARGS: script name, entity info table
+local function PostListGet( name, base, ents )
+    local data = SetupEntityInfo( base, ents )
+    local code = FormatEntityInfo( name, data )
+
+    file.Write( "p2h_auto.txt", code )
+
+    if tobool( GetConVarNumber( "p2h_converter_clipboard" ) ) then
+        SetClipboardText( code )
+    end
+
+    if not WireLib then return end
+    if not tobool( GetConVarNumber( "p2h_converter_openeditor" ) ) then return end
+
+    spawnmenu.ActivateTool( "wire_expression2" )
+
+    openE2Editor()
+    if wire_expression2_editor then
+        wire_expression2_editor:NewTab()
+        wire_expression2_editor:SetCode( code )
+    end
+
+end
+
+-------------------------------------------
+-- FUNC: Recieves data from server
+local function GetListFromServer()
+    local eid = net.ReadUInt( 16 )
+    local base = Entity( eid )
+
+    if not IsValid( base ) then return end
+
+    local ents = { base }
+    local count = net.ReadUInt( 16 )
+
+    for i = 1, count do
+        local eid = net.ReadUInt( 16 )
+        local ent = Entity( eid )
+
+        if not IsValid( ent ) then continue end
+
+        ents[#ents + 1] = ent
+    end
+
+    Derma_StringRequest(
+        "Expression2 Script Name", "Please enter a name for your script!", "default_script_name",
+
+        function ( text )
+            PostListGet( text, base, ents )
+        end,
+
+        function () end
+    )
+end
+net.Receive( "p2h_converter", GetListFromServer )
---
+++ lua/weapons/gmod_tool/stools/p2h_converter.lua
@@ -0,0 +1,247 @@
+
+-------------------------------------------
+-- Prop to holo converter
+-- by shadowscion
+
+AddCSLuaFile( "p2h/convert.lua" )
+
+-------------------------------------------
+-- Shared
+TOOL.Name     = "E2 Hologram Converter"
+TOOL.Category = "Render"
+
+if WireLib then
+    TOOL.Tab                  = "Wire"
+    TOOL.Wire_MultiCategories = { "Tools" }
+end
+
+TOOL.ClientConVar = {
+    ["radius"] = 64,
+    ["clipboard"] = 1,
+    ["openeditor"] = 1,
+}
+
+
+-------------------------------------------
+-- Client
+if CLIENT then
+    include( "weapons/gmod_tool/stools/p2h/convert.lua" )
+
+    language.Add( "Tool.p2h_converter.name", "E2 Hologram Converter" )
+    language.Add( "Tool.p2h_converter.desc", "Converts props into holograms for use with expression2." )
+    language.Add( "Tool.p2h_converter.0", "Click to select or deselect an entity. Hold USE to select entities within a radius. Right click to finalize." )
+
+    TOOL.LeftClick  = function() return true end
+    TOOL.RightClick = function() return true end
+
+    local PANEL_TEXT_COLOR = Color( 0, 0, 0 )
+
+    function TOOL.BuildCPanel( self )
+        -- Base panel
+        self.Paint = function( pnl, w, h )
+            draw.RoundedBox( 0, 0, 0, w, 20, Color( 50, 50, 50, 255 ) )
+            draw.RoundedBox( 0, 1, 1, w - 2, 18, Color( 125, 125, 125, 255 ) )
+        end
+
+        -- Root category element
+        local root = vgui.Create( "DCollapsibleCategory" )
+
+        root:SetLabel( "Prop to Hologram Converter" )
+        root:SetExpanded( 1 )
+        self:AddItem( root )
+
+        root.Paint = function( pnl, w, h )
+            draw.RoundedBox( 0, 0, 0, w, h, Color( 50, 50, 50, 255 ) )
+            draw.RoundedBox( 0, 1, 1, w - 2, h - 2, Color( 175, 175, 175, 255 ) )
+            draw.RoundedBox( 0, 1, 1, w - 2, 18, Color( 125, 125, 125, 255 ) )
+        end
+
+        -- List container
+        local container = vgui.Create( "DPanelList" )
+
+        container:SetAutoSize( true )
+        container:SetDrawBackground( false )
+        container:SetSpacing( 5 )
+        container:SetPadding( 5 )
+        root:SetContents( container )
+
+        ------------------------------------
+        -- Clipboard toggle
+        local xbox = vgui.Create( "DCheckBoxLabel" )
+
+        xbox:SetText( "Copy to clipboard." )
+        xbox:SetTextColor( PANEL_TEXT_COLOR )
+        xbox:SetValue( 1 )
+        xbox:SetConVar( "p2h_converter_clipboard" )
+        container:AddItem( xbox )
+
+        ------------------------------------
+        -- Editor toggle
+        local xbox = vgui.Create( "DCheckBoxLabel" )
+
+        xbox:SetText( "Open in expression2 editor." )
+        xbox:SetTextColor( PANEL_TEXT_COLOR )
+        xbox:SetValue( 1 )
+        xbox:SetConVar( "p2h_converter_openeditor" )
+        container:AddItem( xbox )
+
+        ------------------------------------
+        -- Set selection radius
+        local ctrl = vgui.Create( "DNumSlider" )
+
+        ctrl:SetText( "Selection radius." )
+        ctrl.Label:SetTextColor( PANEL_TEXT_COLOR )
+        ctrl:SetMin( 0 )
+        ctrl:SetMax( 4096 )
+        ctrl:SetDecimals( 0 )
+        ctrl:SetConVar( "p2h_converter_radius" )
+        container:AddItem( ctrl )
+    end
+
+    return
+end
+
+
+-------------------------------------------
+-- Server
+util.AddNetworkString( "p2h_converter" )
+
+TOOL.Selection      = {}
+TOOL.SelectionColor = Color( 0, 255, 0, 125 )
+
+-------------------------------------------
+-- Checks if an entity belongs to a player
+local function IsPropOwner( ply, ent )
+    if CPPI then return ent:CPPIGetOwner() == ply end
+
+    for k, v in pairs( g_SBoxObjects ) do
+        for b, j in pairs( v ) do
+            for _, e in pairs( j ) do
+                if e == ent and k == ply:UniqueID() then return true end
+            end
+        end
+    end
+
+    return false
+end
+
+-------------------------------------------
+-- Checks if an entity is already selected
+function TOOL:IsSelected( ent )
+    return self.Selection[ent] ~= nil
+end
+
+-------------------------------------------
+--  Adds an entity to selection
+function TOOL:Select( ent )
+    if not self:IsSelected( ent ) then
+        local oldColor = ent:GetColor()
+
+        ent:SetColor( self.SelectionColor )
+        ent:SetRenderMode( RENDERMODE_TRANSALPHA )
+        ent:CallOnRemove( "e2holo_convertor_onrmv", function( e )
+            self:Deselect( e )
+            self.Selection[e] = nil
+        end )
+
+        self.Selection[ent] = oldColor
+    end
+end
+
+-------------------------------------------
+-- Removes an entity from selection
+function TOOL:Deselect( ent )
+    if self:IsSelected( ent ) then
+        local oldColor = self.Selection[ent]
+
+        ent:SetColor( oldColor )
+        ent:SetRenderMode( oldColor.a ~= 255 and RENDERMODE_TRANSALPHA or RENDERMODE_NORMAL )
+
+        self.Selection[ent] = nil
+    end
+end
+
+-------------------------------------------
+-- Left click ( selection )
+function TOOL:LeftClick( eye )
+
+    -- Filter out bad entities
+    local user   = self:GetOwner()
+    local hitEnt = eye.Entity
+
+    if not IsValid( user ) or ( hitEnt:IsWorld() and not user:KeyDown( IN_USE ) ) then
+        return false
+    end
+
+    if IsValid( hitEnt ) then
+        if hitEnt:IsPlayer() then return false end
+        if not IsPropOwner( user, hitEnt ) then return false end
+        if not util.IsValidPhysicsObject( hitEnt, eye.PhysicsBone ) then return false end
+    end
+
+    -- Area select
+    if user:KeyDown( IN_USE ) then
+        local radius = math.Clamp( self:GetClientNumber( "radius" ), 64, 4096 )
+
+        for _, ent in pairs( ents.FindInSphere( eye.HitPos, radius ) ) do
+            if not IsValid( ent ) or not IsPropOwner( user, ent ) then continue end
+            self:Select( ent )
+        end
+
+        return false
+    end
+
+    -- Deselect entity if already selected
+    if self:IsSelected( hitEnt ) then self:Deselect( hitEnt ) return false end
+
+    -- Otherwise add to selection
+    self:Select( hitEnt )
+
+    -- No serverside tool sounds
+    return false
+
+end
+
+-------------------------------------------
+-- Right click ( finalize )
+function TOOL:RightClick( eye )
+
+    -- Filter out bad entities
+    local user   = self:GetOwner()
+    local hitEnt = eye.Entity
+
+    if not IsValid( user ) or not IsValid( hitEnt ) or hitEnt:IsWorld() then
+        return false
+    end
+
+    if hitEnt:IsPlayer() then return false end
+    if not IsPropOwner( user, hitEnt ) then return false end
+    if not util.IsValidPhysicsObject( hitEnt, eye.PhysicsBone ) then return false end
+
+    -- Remove base entity from selection
+    self:Deselect( hitEnt )
+
+    -- Send entity list to client
+    local selectionCount = table.Count( self.Selection )
+
+    net.Start( "p2h_converter" )
+        -- Base entity
+        net.WriteUInt( hitEnt:EntIndex(), 16 )
+
+        -- Entity list
+        if selectionCount >= 1 then
+            net.WriteUInt( selectionCount, 16 )
+            for ent, _ in pairs( self.Selection ) do
+                net.WriteUInt( ent:EntIndex(), 16 )
+                self:Deselect( ent )
+            end
+        end
+    net.Send( user )
+
+    -- Clear selection
+    self.Selection = {}
+
+    -- No serverside tool sounds
+    return false
+
+end
